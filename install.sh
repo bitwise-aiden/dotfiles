d4801899d56dfc2b2524f0c276393cbba6e6bd48
Aiden Storey <git@storey.dev>
2024-02-06 19:24:22 -0500

    Remove stream deck

--- install.sh
+++ install.sh
@@ -3,24 +3,13 @@
 mv ~/.zprofile ~/.zprofile-old
 ln -s ~/dotfiles/.zprofile ~/.zprofile
 
-mv ~/Library/Application\ Support/Code/User/settings.json ~/Library/Application\ Support/Code/User/settings-old.json
-ln -s ~/dotfiles/.vscode ~/Library/Application\ Support/Code/User/settings.json
-
 cat ~/dotfiles/.gitalias >> ~/.gitconfig
+cat ~/dotfiles/.gituser >> ~/.gitconfig
+echo -n Z2l0QHN0b3JleS5kZXY= | base64 -D >> ~/.gitconfig
 
 touch ~/dotfiles/command/_local.sh
 
 if ! [ $SPIN ]; then
     mkdir -p ~/dotfiles/script
     mkdir -p ~/dotfiles/data
-
-    working_dir=$PWD
-
-    for plugin in ~/dotfiles/streamdeck/*; do
-        cd $plugin
-        ln -s $plugin ~/Library/Application\ Support/com.elgato.StreamDeck/Plugins/
-        $plugin/install.sh
-    done
-
-    cd $working_dir
 fi
--- streamdeck/com.aiden.productivity.sdPlugin/install.sh
+++
@@ -1,4 +0,0 @@
-python3 -m venv venv
-. ./venv/bin/activate
-python -m pip install -r requirements.txt
-pyinstaller productivity.py --onefile
